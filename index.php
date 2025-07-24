77dc32384e8fc05aac55244ed56c23f7519567cf
chandanayr03 <139262783+chandanayr03@users.noreply.github.com>
2025-07-24 20:32:40 +0530

    Initial commit - added Student Management System

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+  <!--Start of Tawk.to Script-->
+<!-- <script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/630c733e37898912e965e64a/1gbka4bnc';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script> -->
+<!--End of Tawk.to Script-->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Student Insight</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Cabin:700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+	<style>
+.dropbtn {
+    background-color: white;
+    color: black;
+    padding: 14px;
+    font-size: 14px;
+    border: none;
+    cursor: pointer;
+}
+
+.dropbtn:hover, .dropbtn:focus {
+    background-color: blue;
+}
+
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: blue;
+    min-width: 160px;
+    overflow: auto;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown a:hover {background-color: #ddd}
+
+.show {display:block;}
+</style>
+<style>
+        body {
+            margin: 0;
+            overflow: hidden;
+            
+
+  
+
+
+        }
+        video {
+            object-fit: cover;
+            width: 100vw;
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+
+        body
+        {
+          background-image: url('image.jpeg');
+        }
+        
+
+        .content {
+            position: relative;
+            z-index: 1;
+            color: #ffffffff;
+            text-align: center;
+            padding: 100px;
+        }
+    </style>
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <header class="masthead">
+      <div class="intro-body">
+      
+        <div class="container">
+        <video autoplay muted loop>
+        <source src="assets/img/SI.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <div class="content">
+    <div class="row">
+            <div class="col-lg-12 mx-auto">
+              <h4 class="brand-heading" style="color:white; size:10px;"> STUDENT INSIGHT</h4>
+              
+              <a href="#about" class="btn btn-circle js-scroll-trigger">
+                <i class="fa fa-angle-double-down animated"></i>
+              </a>
+            </div>
+          </div>
+    </div>
+          
+        </div>
+      </div>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top" style="color:#2980B9;">Student Insight</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#download">Services</a>
+            </li>
+			 
+			
+            <li class="nav-item">
+              <button onclick="myFunction()" class="dropbtn"  class="nav-link js-scroll-trigger">SIGN-UP</button>
+  <div id="myDropdown" class="dropdown-content">
+  <a href="adminlogin.php"> ADMIN</a>
+  <a href="facultylogin.php">FACULTY</a>
+  <a href="studentlogin.php">STUDENT</a>
+    
+    
+	
+  </div>
+            </li>  
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="masthead">
+      <div class="intro-body">
+        <div class="container">
+        <background-image: url('\Student_Insight\assets\img\WhatsApp Image 2024-05-13 at 1.29.49 PM.jpeg');>
+    <div class="content">
+    <div class="row">
+            <div class="col-lg-12 mx-auto">
+              <h1 class="brand-heading" style="color:orange;"> Student Insight</h1>
+              <p>Welcome to our student_insights website, From tracking grades and assignments to organizing schedules. Join us today and experience the convenience of effective student management at your fingertips.</p>
+              
+              <a href="#about" class="btn btn-circle js-scroll-trigger">
+                <i class="fa fa-angle-double-down animated"></i>
+              </a>
+            </div>
+          </div>
+    </div>
+          
+        </div>
+      </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="content-section text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 mx-auto">
+            <h2 style="color:orange;">ABOUT Student Insight</h2>
+            Welcome to our student_insights website, From tracking grades and assignments to organizing schedules. Join us today and experience the convenience of effective student management at your fingertips.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Download Section -->
+    <section id="download" class="download-section content-section text-center">
+      <div class="container" style="padding-bottom:100px;">
+        <div class="col-lg-12 mx-auto">
+          <h2 style="color:orange;">OUR SERIVICES</h2>
+         
+          <a href="">STUDENT</a>
+		   <p>Welcome to our student_insights website, From tracking grades and assignments to organizing schedules. Join us today and experience the convenience of effective student management at your fingertips. </p>
+		   <a href="">FACULTY</a>
+		   
+		   <a href="">ADMIN</a>
+		   <p>COLLEGE ADMIN CAN VIEW STUDENTS AND FACULTY DETAILS </p>
+        </div>
+      </div>
+    </section>
+
+    <!-- Contact Section -->
+    
+	<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
+
+    <!-- Map Section -->
+    <div id="map"></div>
+
+    <!-- Footer -->
+    
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+   
+
+    <!-- Custom scripts for this template -->
+    <script src="js/grayscale.min.js"></script>
+
+  </body>
+
+</html>
